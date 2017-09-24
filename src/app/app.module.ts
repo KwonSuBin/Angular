771d834f28bda01d0b4ad2ccb33a87aa72aa78de
KwonSuBin <ksb971118@gmail.com>
2017-09-24 20:43:45 +0900

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { TestTopnavComponent } from './test-topnav/test-topnav.component';
+import { Test1Component } from './test-topnav/test1/test1.component';
+import { Test2Component } from './test-topnav/test2/test2.component';
+import { Test3Component } from './test-topnav/test3/test3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    TestTopnavComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
