8f303607ba1800546904a4b8a9a58b4cdfe02ca9
KwonSuBin <ksb971118@gmail.com>
2017-09-25 18:12:25 +0900

    4

--- src/app/test-topnav/test-topnav.component.ts
+++ src/app/test-topnav/test-topnav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Test1Component } from '../test1/test1.Component';
+import {Test2Component } from '../test2/test2.Component';
+import {Test3Component } from '../test3/test3.Component';
+
 
 @Component({
   selector: 'app-test-topnav',
